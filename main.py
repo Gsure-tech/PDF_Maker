8bf1cd0a09681dd1a174f5e123a51b3611c2cc7c
gsure-tech <Geeescode@>
2025-01-11 17:13:58 +0100

    create pdf from csv

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
+import pandas as pd
 from fpdf import FPDF
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1)
+for index, row in df.iterrows():
 
-pdf.set_font(family="Times",size=12)
-pdf.cell(w=0, h=12, txt="Welcome There!", align="L", ln=1)
+    pdf.add_page()
 
-pdf.output("output.pdf")
-
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L", ln=1)
-
-pdf.set_font(family="Times",size=12)
-pdf.cell(w=0, h=12, txt="Welcome There!", align="L", ln=1)
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 
 pdf.output("output.pdf")
